89c6ed244d7b9b322ad0a19f4d14825b0caaa0b2
tprudente <62073547+tprudente@users.noreply.github.com>
2021-01-24 00:32:43 +0000

    added comments to unpacking

--- testes/format_strings.py
+++ testes/format_strings.py
@@ -4,6 +4,8 @@ var2 = 'world'
 msg = f'the first {var1} to the {var2} do {v_input}'
 print(msg)
 print(f'outra maneira de dizer {var1} {var2}')
-unpacking = ('T','O','m')
-a,b,c = unpacking
+
+#unpacking serve para atribuir a variaveis os valores de uma lista por ex
+unpacking = ('T','O','m') #lista
+a,b,c = unpacking       #a = unpacking[0] etc...
 print(F'{a} {b} {c.upper()}')
